4a00c11595ad8eb0a33966f9d7c49643d9620fd0
David Sires <d.sires@yahoo.com>
2017-03-09 19:12:32 -0800

    Assignment-7-Services2

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -2,6 +2,10 @@
     function SongPlayer() {
         var SongPlayer = {};
         
+        /**
+        * @desc currentSong object ensures song is reset at page load
+        * @type {object}
+        */
         var currentSong = null;
          /**
          * @desc Buzz object audio file
@@ -27,23 +31,43 @@
             
             currentSong = song;
         };
-        
+        /** 
+        * @function playSong
+        * @desc plays the song that is clicked updates the album.html to show true and changes the play/pause icon
+        * @param {object} song
+        */
+        var playSong function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        }
+        /** 
+        * @function pauseSong
+        * @desc pauses the song that is clicked updates the album.html to show false and changes the play/pause icon
+        * @param {object} song
+        */
+        var pauseSong function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
+        }
+        /** 
+        * @function SongPlayer.play
+        * @desc plays the currently selected song and stops the currently selected song depending on if the play or pause button is selected
+        * @param {object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play(); 
-                song.playing = true;
+                playSong(song);
                 
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
         
         SongPlayer.pause = function(song) {
-            currentBuzzObject.pause();
-            song.playing = false;
+            pauseSong(song);
         };
         
         return SongPlayer;
